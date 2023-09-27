2291107c5e50773ec55aa7a82ff8290add4e6c25
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-09-26 23:45:53 -0300

    mais exercicios criados

---
+++ 4-d.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    double pid;
+    double pi = 3.14159265358979323846;
+    
+    pid = pi;
+
+    cout << pid << endl;
+    
+    
+    return 0;
+}
+
---
+++ 4-e.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    //double num1 = 1.0e100;
+    //long double num2 = 1.0e100;
+
+    double num1;
+    long double num2;
+
+    double valorMaximo = numeric_limits<double>::max();
+    num1 = valorMaximo;
+    num2 = valorMaximo;
+
+    cout << "Resultado do double: " << num1 * 2.0 << endl;
+    cout << "Resultado do long double: " << num2 * 2.0 << endl;
+
+    return 0;
+}
+
+/*
+* O tipo double é utilizado para representar números de ponto flutuante de precisão simples.
+* O tipo long double é utilizado para representar números de ponto flutuante de precisão estendida, 
+  que oferece maior precisão em comparação com double.
+ -> O resultado acima demonstra: o double resulta em "infinito" (inf) devido ao estouro da faixa de representação, 
+  enquanto long double pode acomodar o valor.
+
+
+*/
